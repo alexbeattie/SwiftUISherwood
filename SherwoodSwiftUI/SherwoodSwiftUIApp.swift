b012f56837ab1a5a013630ae4fa03bc842e2a961
Alex Beattie <artrisanbranding@me.com>
2020-12-14 14:29:57 -0800

    Initial Commit

---
+++ SherwoodSwiftUI/SherwoodSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SherwoodSwiftUIApp.swift
+//  SherwoodSwiftUI
+//
+//  Created by Alex Beattie on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct SherwoodSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
