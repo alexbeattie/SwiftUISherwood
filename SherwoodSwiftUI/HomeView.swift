88aa361153df3b8973b7c9e279148ef34d501fdf
Alex Beattie <artrisanbranding@me.com>
2020-12-25 12:58:56 -0800

    working images

--- SherwoodSwiftUI/HomeView.swift
+++ SherwoodSwiftUI/HomeView.swift
@@ -19,54 +19,54 @@ struct QueryResult: Decodable, Hashable {
     var ResourceUri: String
     var StandardFields: Fields
 
-    struct Fields: Decodable, Hashable {
-        var BuildingAreaTotal: Float
-        let Latitude: Double
-        let Longitude: Double
-        var ListingId: String
-        var ListAgentName: String
-        var CoListAgentName: String
-        var MlsStatus: String
-        var ListOfficePhone: String
-        
-        var UnparsedFirstLineAddress: String
-        var City: String
-        var PostalCode: String
-        var StateOrProvince: String
-        
-        var CurrentPricePublic: Int
-//        var PublicRemarks: String?
-      
-        var VirtualTours: [VirtualToursObjs]?
-        struct VirtualToursObjs: Codable, Hashable {
-            var Uri: String?
-        }
-        var Videos: [VideosObjs]?
-        struct VideosObjs: Codable, Hashable {
-            var ObjectHtml: String?
-        }
-//
-        var Documents: [DocumentsAvailable]?
-        struct DocumentsAvailable: Codable, Hashable {
-            var Id: String
-            var ResourceId: String
-            var Name: String
-        }
-        var Photos: [PhotoDictionary]?
-        struct PhotoDictionary: Codable, Hashable {
-            var Id: String
-            var Name: String
-            var Uri300: String
-
-        }
-    }
+    
 
 }
+struct Fields: Decodable, Hashable {
+    var BuildingAreaTotal: Float
+    let Latitude: Double
+    let Longitude: Double
+    var ListingId: String
+    var ListAgentName: String
+    var CoListAgentName: String
+    var MlsStatus: String
+    var ListOfficePhone: String
+    
+    var UnparsedFirstLineAddress: String
+    var City: String
+    var PostalCode: String
+    var StateOrProvince: String
+    
+    var CurrentPricePublic: Int
+//        var PublicRemarks: String?
+  
+    var VirtualTours: [VirtualToursObjs]?
+    struct VirtualToursObjs: Codable, Hashable {
+        var Uri: String?
+    }
+    var Videos: [VideosObjs]?
+    struct VideosObjs: Codable, Hashable {
+        var ObjectHtml: String?
+    }
+//
+    var Documents: [DocumentsAvailable]?
+    struct DocumentsAvailable: Codable, Hashable {
+        var Id: String
+        var ResourceId: String
+        var Name: String
+    }
+    var Photos: [PhotoDictionary]?
+    struct PhotoDictionary: Codable, Hashable {
+        var Id: String
+        var Name: String
+        var Uri300: String
 
+    }
+}
 class HomeViewModel: ObservableObject {
     @Published var items = 0..<5
     @Published var listingResults = [QueryResult]()
-    @Published var listingFields = [QueryResult.Fields]()
+//    @Published var listingFields = [QueryResult.Fields]()
 //    @Published var fields = [Fields]()
     init() {
         guard let url = URL(string: "http://artisanbranding.com/test.json") else { return }
@@ -98,34 +98,28 @@ struct HomeView: View {
     var body: some View {
         NavigationView{
             ScrollView {
-                
-                LazyVGrid(columns: [
-                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 16, alignment: .top),
-                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 16, alignment: .top),
-                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 16),
-                ],alignment: .leading, spacing: 16, content: {
-            
+                VStack(spacing: 4) {
                     ForEach(vm.listingResults, id: \.self) { num in
-//                        print(num.)
-                        VStack(alignment:.leading, spacing: 4) {
-                            KFImage(URL(string: "num.listingResults"))
-                                
+                            KFImage(URL(string:num.StandardFields.Photos?.first?.Uri300 ?? ""))
                                 .resizable()
-                                .scaledToFit()
-                                .cornerRadius(22)
+                                .scaledToFill()
+                                .frame(height:200)
+                                .clipped()
+
                             
-                            Text(num.StandardFields.MlsStatus).font(.system(size: 20, weight: .semibold)).padding(4 )
                             Text(num.StandardFields.UnparsedFirstLineAddress).font(.system(size: 16, weight: .regular))
                             Text("\(num.StandardFields.CurrentPricePublic)").font(.system(size: 12, weight: .regular)).foregroundColor(.black)
-                            Spacer()
+                        Text(num.StandardFields.MlsStatus).font(.system(size: 20, weight: .semibold)).padding(4 )
+//                            Spacer()
                         }
 //                        .padding()
                       //.background(Color.blue)
                     }
                     
-                }).padding(.horizontal)
                 
-            }.navigationTitle("gridsearch")
+            .padding(.horizontal)
+                
+            }.navigationTitle("Listings")
         }
     }
 }
